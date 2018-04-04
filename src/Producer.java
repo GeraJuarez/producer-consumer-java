c9db853d520562855c82db5375440c26bdfb3c8d
geraJuarez <geratrex781@gmail.com>
2018-04-04 13:29:49 -0500

    Producer produced random prefix operations
    To do:
    * Check if the user selects the range of numbers inside the operation
    * Parse the operations

--- src/Producer.java
+++ src/Producer.java
@@ -10,6 +10,12 @@ import java.util.logging.Logger;
 public class Producer extends Thread {
     Buffer buffer;
     String name;
+    
+    // Constant indices for the production of operations
+    final int operatorPos = 1;
+    final int firstNumberPos = 3;
+    final int secondNumberPos = 5;
+    final String operations = "+-*/";
 
     public Producer(Buffer buffer, String name) {
         this.buffer = buffer;
@@ -19,13 +25,11 @@ public class Producer extends Thread {
     @Override
     public void run() {
         System.out.println("Running producer... ");
-        String products = "AEIOU";
-        Random r = new Random();
-        char product = 0;
+        String product;
         
         while (true) {
-            product = products.charAt(r.nextInt(5));
-            this.buffer.produceQ(Character.toString(product) + this.name);
+            product = producePrefixOperation();
+            this.buffer.produceQ(product + " : " + this.name);
             System.out.println("Producer " + this.name + " produced: " + product);
             
             try {
@@ -36,4 +40,32 @@ public class Producer extends Thread {
         }
     }
     
+    private String producePrefixOperation() {
+        // Create operations with this form: (operation n1 n2)
+        String prefixOp = "";
+        
+        /* -- */
+        int NumberRange = 10; // This might be passed by a parameter
+        /* -- */
+        
+        // Init random
+        Random opRand = new Random();
+        Random rangeRand = new Random();
+        
+        // init default operation
+        StringBuilder sb = new StringBuilder("(+ 1 2)");
+        
+        // Get random operator and replace
+        String tempOperation = Character.toString( operations.charAt(opRand.nextInt(4)) );
+        sb.replace(operatorPos, operatorPos + 1 , tempOperation);
+        
+        // Get random number and replace
+        String tempNumber = "" + rangeRand.nextInt(NumberRange);
+        sb.replace(firstNumberPos, firstNumberPos + 1, tempNumber);
+        tempNumber = "" + rangeRand.nextInt(NumberRange);
+        sb.replace(secondNumberPos, secondNumberPos + 1, tempNumber);
+        
+        prefixOp = sb.toString(); 
+        return prefixOp;
+    }
 }
